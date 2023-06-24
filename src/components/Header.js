a7ee006443826f5668cd38deb83eb8729eb41fd6
KusalNayanajith99 <kusalnayanajith88@gmail.com>
2023-06-24 20:19:59 +0530

    bugFix3

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,9 +7,9 @@ const Header = () => {
     <div className='container mx-auto'>
       <div className='flex justify-between items-center'>
         {/* logo */}
-        <a herf="/">
-          <img src={Logo} alt='image' />
-        </a>
+        {/* <a herf="/"> */}
+          <img src={Logo} alt='image12' />
+        {/* </a> */}
         {/* button */}
         <button className='btn btn-sm'>Work with me</button>
       </div>
